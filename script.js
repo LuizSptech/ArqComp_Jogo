63c0a3bda9e289cf46623ecbdbdde1c0ccf27031
LuizSptech <luiz.correia@sptech.school>
2026-05-28 16:47:41 -0300

    movimentação

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const player = document.querySelector(".player")
 
 function attack() {
 
-    player.style.left = "370px"
+    player.style.transform = "translate(1198px, 400px)";
+    console.log('isso')
+
 
     setTimeout(() => {
-        player.style.left = "100px"
-    }, 600)
-
-}
+        player.style.transform = "translate(-1px, -1px)";
+        console.log('entrou')
+    },600)
+    }
+   
