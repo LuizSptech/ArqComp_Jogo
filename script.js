8a6a357a3043074e0a24ef37bfdb77fd0ed1b51a
LuizSptech <luiz.correia@sptech.school>
2026-05-29 13:07:25 -0300

    animação letra por letra

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-const player = document.querySelector(".player")
+const player = document.querySelector(".player");
+let tamanho = 100;
+let distancia = 1198;
+const velocidade = 120;
+const meuTexto = "EITCHA";
+const elementoAlvo = document.getElementById("h1");
 
-let tamanho = 100 
-let distancia = 1198
+function efeitoDigitacao(elemento, texto, intervalo) {
+  let j = 0;
+  const timer = setInterval(() => {
+    elemento.innerHTML += texto.charAt(j); 
+    j++;
+    if (j >= texto.length) {              
+      clearInterval(timer);
+    }
+  }, intervalo);
+}
+
 function attack() {
+  efeitoDigitacao(elementoAlvo, meuTexto, velocidade);
 
+  player.style.width = `${tamanho}px`;
+  player.style.height = `${tamanho}px`;
+  player.style.transform = "translate(850px, 400px)";
+  console.log('isso');
 
-
-    for(let i = 0; i < distancia; i++){
-        player.style.width = `${tamanho}px`;
-        player.style.height = `${tamanho}px`
-    }
-
-    player.style.transform = "translate(850px, 400px)";
-    console.log('isso')
-
-
-    setTimeout(() => {
-        player.style.transform = "translate(-1px, -1px)";
-        player.style.width = "50px";
-        player.style.height = "50px";
-        
-        console.log('entrou')
-    },600)
-
-
-    }
+  setTimeout(() => {
+    player.style.transform = "translate(-1px, -1px)";
+    player.style.width = "50px";
+    player.style.height = "50px";
+    console.log('entrou');
+  }, 600);
+}
    
