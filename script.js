11f8b2d56e4b8db1a1924239211027c50cb64d71
LuizSptech <luiz.correia@sptech.school>
2026-05-28 22:38:33 -0300

    fundo-adicionado-movimento-animação

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const player = document.querySelector(".player")
 
+let tamanho = 100 
+let distancia = 1198
 function attack() {
 
-    player.style.transform = "translate(1198px, 400px)";
+
+
+    for(let i = 0; i < distancia; i++){
+        player.style.width = `${tamanho}px`;
+        player.style.height = `${tamanho}px`
+    }
+
+    player.style.transform = "translate(850px, 400px)";
     console.log('isso')
 
 
     setTimeout(() => {
         player.style.transform = "translate(-1px, -1px)";
+        player.style.width = "50px";
+        player.style.height = "50px";
+        
         console.log('entrou')
     },600)
+
+
     }
    
